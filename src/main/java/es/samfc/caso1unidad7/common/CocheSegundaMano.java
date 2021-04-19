785ca2dfbded2813b53bc0530c6d994e318155a3
Samuel Fernandez Cardenas <samuelxpxp1999@gmail.com>
2021-04-19 23:04:41 +0200

    Documentacion de la clase CocheSegundaMano

--- src/main/java/es/samfc/caso1unidad7/common/CocheSegundaMano.java
+++ src/main/java/es/samfc/caso1unidad7/common/CocheSegundaMano.java
@@ -1,37 +1,66 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.samfc.caso1unidad7.common;
 
+
 /**
- *
- * @author samue
+ * Clase de coches de segunda mano.
+ * 
+ * Esta clase, que hereda todos los atributos y metodos de la 
+ * clase {@link Coche Coche.class}, se usara para coches
+ * de segunda mano. Incluye metodos adicionales como el
+ * kilometraje y el propietario anterior.
+ * 
+ * @author Samuel Fernandez Cardenas
  */
 public class CocheSegundaMano extends Coche {
     
     private int kilometros;
     private String propietarioAnterior;
 
+    
+    /**
+     * Constructor de la clase CocheNuevo.
+     * 
+     * @param matricula Matricula que tendra el coche
+     * @param modelo Modelo del coche
+     * @param color Color del coche
+     * @param kilometros Kilometraje del coche  
+     * @param propietarioAnterior Propietario anterior del vehiculo
+     * 
+     */
     public CocheSegundaMano(String matricula, String modelo, String color, int kilometros, String propietarioAnterior) {
         super(matricula, modelo, color);
         this.kilometros = kilometros;
         this.propietarioAnterior = propietarioAnterior;
     }
 
+    /**
+     * Metodo de obtencion de kilometraje
+     * @return Kilometros del coche
+     */
     public int getKilometros() {
         return kilometros;
     }
 
+    /**
+     * Metodo de definicion de kilometraje
+     * @param kilometros Kilometros del coche
+     */
     public void setKilometros(int kilometros) {
         this.kilometros = kilometros;
     }
 
+    /**
+     * Metodo de obtencion de propietario anterior
+     * @return Propietario anterior del vehiculo
+     */
     public String getPropietarioAnterior() {
         return propietarioAnterior;
     }
 
+    /**
+     * Metodo de definicion de propietario anterior
+     * @param propietarioAnterior Propietario anterior del vehiculo
+     */
     public void setPropietarioAnterior(String propietarioAnterior) {
         this.propietarioAnterior = propietarioAnterior;
     }
