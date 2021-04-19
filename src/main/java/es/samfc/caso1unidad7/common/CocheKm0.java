d0aeb656c0c5efdced17b1c630f386c00b0c0ffe
Samuel Fernandez Cardenas <samuelxpxp1999@gmail.com>
2021-04-19 18:49:48 +0200

    Documentacion de la clase CocheKm0

--- src/main/java/es/samfc/caso1unidad7/common/CocheKm0.java
+++ src/main/java/es/samfc/caso1unidad7/common/CocheKm0.java
@@ -1,27 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.samfc.caso1unidad7.common;
 
 /**
- *
- * @author samue
+ * Clase de coches de KM0.
+ * 
+ * Esta clase, que hereda todos los atributos y metodos de la 
+ * clase {@link Coche Coche.class}, se usara para coches
+ * de kilometro 0, incluyendo un nuevo atributo que almacena
+ * los kilometros que tiene el vehiculo.
+ * 
+ * @author Samuel Fernandez Cardenas
  */
 public class CocheKm0 extends Coche {
     
     private int kilometros;
 
+    /**
+     * Constructor de la clase CocheKm0.
+     * 
+     * @param matricula Matricula que tendra el coche
+     * @param modelo Modelo del coche
+     * @param color Color del coche
+     * @param kilometros Kilometros del coche
+     */
     public CocheKm0(String matricula, String modelo, String color, int kilometros) {
         super(matricula, modelo, color);
         this.kilometros = kilometros;
     }
 
+    /**
+     * Metodo de obtencion de kilometraje
+     * @return Kilometros del coche
+     */
     public int getKilometros() {
         return kilometros;
     }
 
+    /**
+     * Metodo de definicion de kilometraje
+     * @param kilometros Kilometros del coche
+     */
     public void setKilometros(int kilometros) {
         this.kilometros = kilometros;
     }
