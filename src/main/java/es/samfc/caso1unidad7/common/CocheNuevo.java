4dd31febbc4ca4fb247efc0e3d7296b45b5c6220
Samuel Fernandez Cardenas <samuelxpxp1999@gmail.com>
2021-04-19 18:52:02 +0200

    Documentacion de la clase CocheNuevo

--- src/main/java/es/samfc/caso1unidad7/common/CocheNuevo.java
+++ src/main/java/es/samfc/caso1unidad7/common/CocheNuevo.java
@@ -1,16 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package es.samfc.caso1unidad7.common;
 
 /**
- *
- * @author samue
+ * Clase de coches nuevos.
+ * 
+ * Esta clase, que hereda todos los atributos y metodos de la 
+ * clase {@link Coche Coche.class}, se usara para coches
+ * nuevos.
+ * 
+ * @author Samuel Fernandez Cardenas
  */
 public class CocheNuevo extends Coche {
     
+    /**
+     * Constructor de la clase CocheNuevo.
+     * 
+     * @param matricula Matricula que tendra el coche
+     * @param modelo Modelo del coche
+     * @param color Color del coche
+     */
     public CocheNuevo(String matricula, String modelo, String color) {
         super(matricula, modelo, color);
     }
